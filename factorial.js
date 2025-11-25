a66196408278fecda1dc955691b371f2565f0318
PSaranyadotcom <152602711+PSaranyadotcom@users.noreply.github.com>
2025-11-25 13:49:13 +0530

    changed factorial.js
    Refactor factorial function to handle negative inputs and improve user input handling.

--- factorial.js
+++ factorial.js
@@ -1,24 +1,39 @@
 
-function factorial(n){
+// function factorial(n){
     
-    if (n === 0) {
-        return 1;
-    }
-     else {
+//     if (n === 0) {
+//         return 1;
+//     }
+//      else {
         
-        return n * factorial(n - 1);
-    }
+//         return n * factorial(n - 1);
+//     }
+// }
+
+
+// //let num = prompt("enter no:"); 
+// var num = prompt("Enter no:");
+// var fact = factorial(num);
+// // Using prompt to get user input
+
+
+// // Displaying the user input
+
+
+
+// console.log(`The factorial of ${num} is: ${fact}`);
+const prompt = require("prompt-sync")();
+
+let num = Number(prompt("Enter a number: "));
+
+function factorial(n) {
+  if (n < 0) {
+    return "Factorial not possible for negative numbers";
+  }
+  if (n === 0 || n === 1) {
+    return 1; // base case
+  }
+  return n * factorial(n - 1); // recursive call
 }
 
-
-//let num = prompt("enter no:"); 
-var num = prompt("Enter no:");
-var fact = factorial(num);
-// Using prompt to get user input
-
-
-// Displaying the user input
-
-
-
-console.log(`The factorial of ${num} is: ${fact}`);
+console.log("Factorial of " + num + " is: " + factorial(num));
